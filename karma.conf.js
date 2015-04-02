4beccffd4371289fda1f7cc4bc7d220c377e45a9
Tilman Potthof <tilmanpotthof@googlemail.com>
2015-04-02 04:14:00 +0200

    test(stop-overrides): added stop-anguler-overrides for tests

--- karma.conf.js
+++ karma.conf.js
@@ -8,14 +8,17 @@ module.exports = function(config) {
     files: [
       'bower_components/angular/angular.js',
       'bower_components/angular-mocks/angular-mocks.js',
-      'src/**.js'
+      'bower_components/stop-angular-overrides/stop-angular-overrides.js',
+      'src/**/**.module.js',
+      'src/**/**.service.js',
+      'src/**/**.spec.js'
     ],
     autoWatch: false,
     browsers: ['PhantomJS'],
     singleRun: true,
     reporters: ['progress', 'coverage'],
     preprocessors: {
-      'src/!(*spec).js': 'coverage'
+      'src/**/!(*spec).js': 'coverage'
     },
     coverageReporter: {
       reporters: [
